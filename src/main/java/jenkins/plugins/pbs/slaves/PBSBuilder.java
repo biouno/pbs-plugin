4d144d258e2007f5f45cf7a14c28bd245ef44ec0
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2013-11-10 02:38:37 -0200

    Adding support to monitor PBS jobs

--- src/main/java/jenkins/plugins/pbs/slaves/PBSBuilder.java
+++ src/main/java/jenkins/plugins/pbs/slaves/PBSBuilder.java
@@ -27,6 +27,7 @@ import hudson.AbortException;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.BuildListener;
+import hudson.model.Result;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.remoting.Callable;
@@ -36,10 +37,18 @@ import hudson.tasks.Builder;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import net.sf.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 
 import com.tupilabs.pbs.PBS;
+import com.tupilabs.pbs.util.CommandOutput;
 import com.tupilabs.pbs.util.PBSException;
 
 /**
@@ -51,6 +60,9 @@ public class PBSBuilder extends Builder {
 
 	@Extension
 	public static final PBSBuilderDescriptor descriptor = new PBSBuilderDescriptor();
+	
+	private static final String REGEX_JOB_STATUS = "dequeuing .*, state (.*)";
+	private static final Pattern JOB_STATUS_REGEX = Pattern.compile(REGEX_JOB_STATUS);
 	
 	/**
 	 * PBS script.
@@ -76,7 +88,10 @@ public class PBSBuilder extends Builder {
 		// TODO: qsub a script
 		listener.getLogger().println("Submitting PBS job...");
 		
-		SubmitPbsJob submit = new SubmitPbsJob(getScript(), listener);
+		int numberOfDays = ((PBSBuilderDescriptor)this.getDescriptor()).getNumberOfDays();
+		int span = ((PBSBuilderDescriptor)this.getDescriptor()).getSpan();
+		
+		SubmitPbsJob submit = new SubmitPbsJob(getScript(), numberOfDays, span, listener);
 		try {
 			launcher.getChannel().call(submit);
 		} catch (PBSException e) {
@@ -86,19 +101,23 @@ public class PBSBuilder extends Builder {
 		return true;
 	}
 	
-	protected static final class SubmitPbsJob implements Callable<Void, PBSException> {
+	protected static final class SubmitPbsJob implements Callable<Result, PBSException> {
 
 		private static final long serialVersionUID = -8294426519319612072L;
 		
 		private final String script;
+		private final int numberOfDays;
+		private final long span;
 		private final BuildListener listener;
 
-		public SubmitPbsJob(String script, BuildListener listener) {
+		public SubmitPbsJob(String script, int numberOfDays, long span, BuildListener listener) {
 			this.script = script;
+			this.numberOfDays = numberOfDays;
+			this.span = span;
 			this.listener = listener;
 		}
 		
-		public Void call() {
+		public Result call() {
 			FileWriter writer = null;
 			try {
 				File tmpScript = File.createTempFile("pbs", "script");
@@ -107,7 +126,10 @@ public class PBSBuilder extends Builder {
 				writer.flush();
 				writer.close();
 				listener.getLogger().println("PBS script: " + tmpScript.getAbsolutePath());
-				PBS.qsub(tmpScript.getAbsolutePath());
+				String jobId = PBS.qsub(tmpScript.getAbsolutePath());
+				
+				Result result = this.seekEnd(listener.getLogger(), jobId, numberOfDays, span);
+				return result;
 			} catch (IOException e) {
 				throw new PBSException("Failed to create temp script");
 			} finally {
@@ -118,13 +140,54 @@ public class PBSBuilder extends Builder {
 					e.printStackTrace();
 				}
 			}
-			return null;
+		}
+
+		// adapted from https://github.com/jenkinsci/call-remote-job-plugin/blob/master/src/main/java/org/ukiuni/callOtherJenkins/CallOtherJenkins/JenkinsRemoteIF.java
+		private Result seekEnd(PrintStream logger, String jobId, int numberOfDays, long span) {
+			CommandOutput cmd = PBS.traceJob(jobId, numberOfDays);
+			
+			String out = cmd.getOutput();
+			String err = cmd.getError();
+			
+			if (StringUtils.isBlank(out)) {
+				listener.getLogger().println("Could not find job " + jobId + " in PBS logs...Marking build as UNSTABLE");
+				listener.getLogger().println(err);
+				return Result.UNSTABLE;
+			}
+			
+			listener.getLogger().println("Seeking job end...");
+			while (true) {
+				Matcher matcher = JOB_STATUS_REGEX.matcher(out);
+				if (matcher.matches()) {
+					String state = matcher.group(1);
+					listener.getLogger().println("Found job state " + state);
+					if ("COMPLETE".equals(state))
+						return Result.SUCCESS;
+					return Result.UNSTABLE;
+				}
+			}
 		}
 		
 	}
 	
 	public static final class PBSBuilderDescriptor extends BuildStepDescriptor<Builder> {
 
+		private Integer numberOfDays;
+		private Integer span;
+		
+		public PBSBuilderDescriptor() {
+			super();
+			load();
+		}
+		
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject json)
+				throws hudson.model.Descriptor.FormException {
+			this.numberOfDays = json.getInt("numberOfDays");
+			this.span = json.getInt("span");
+			return true;
+		}
+		
 		@SuppressWarnings("rawtypes") // Jenkins API
 		@Override
 		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
@@ -134,6 +197,18 @@ public class PBSBuilder extends Builder {
 		@Override
 		public String getDisplayName() {
 			return "Submit PBS job";
+		}
+		
+		public Integer getNumberOfDays() {
+			if (this.numberOfDays == null || this.numberOfDays < 0)
+				return new Integer(1);
+			return this.numberOfDays;
+		}
+		
+		public Integer getSpan() {
+			if (this.span == null || this.span < 0)
+				return new Integer(30);
+			return this.span;
 		}
 		
 	}
